98d93dbf88d8fb2fdf2015a878bcdba7076c6c28
NikhilPeddireddy <139708292+NikhilPeddireddy@users.noreply.github.com>
2024-02-03 00:22:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// your code here
+ function updateURL() {
+
+      const nameValue = document.getElementById('name').value;
+      const yearValue = document.getElementById('year').value;
+
+      const queryString = `?name=${encodeURIComponent(nameValue)}&year=${encodeURIComponent(yearValue)}`;
+      document.getElementById('url').textContent = `https://localhost:8080/${queryString}`;
+    }
+    document.getElementById('button').addEventListener('click', updateURL);
+    
